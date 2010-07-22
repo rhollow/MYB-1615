33f6dea648801109322582fc25baeaffdef28b30
John King <johnk@media.berkeley.edu>
2010-07-21 20:03:09 -0700

    fixes to get round trip notice searching and updating in widget

--- devwidgets/myreminders/javascript/myreminders.js
+++ devwidgets/myreminders/javascript/myreminders.js
@@ -19,6 +19,10 @@
 
 var sakai = sakai || {};
 
+// move this to the global config.js ??
+// the query at this URL will get sakai:type='notice' of sakai:category='reminder' with taskState as a request parameter
+sakai.config.URL.MYREMINDERS_TASKSTATE_SERVICE = "/var/message/notice/reminder_taskstate.json";
+
 /**
  * Initialize the My Reminders widget
  * @param {String} tuid unique id of the widget
@@ -33,7 +37,7 @@ sakai.myreminders = function(tuid, showSettings){
     // Template
     var myremindersTemplate = "myreminders_template";
     
-    var reminders = {
+    var mockreminders = {
         items: 25,
         total: 3,
         results: [{
@@ -47,12 +51,13 @@ sakai.myreminders = function(tuid, showSettings){
             sakai__id: "70896405574174eb091b85ee6be93d4f70558454",
             jcr__primaryType: "",
             sakai__from: "Susan Hagstrom",
-            sakai__subject: "5th week deadline is approaching",
+            "sakai:subject": "5th week deadline is approaching",
             sakai__body: "Dear CED Undergraduate Student,",
             sakai__read: true,
             sakai__sendstate: "notified",
+            "sakai:taskstate": "created",
             sakai__to: "internal:eli",
-            sakai__dueDate: "2010-09-12T06:22:46-07:00",
+            "sakai:dueDate": "2010-09-12T06:22:46-07:00",
             sakai__completeDate: "",
             _charset_: "utf-8",
             id: "70896405574174eb091b85ee6be93d4f70558454",
@@ -92,12 +97,13 @@ sakai.myreminders = function(tuid, showSettings){
             sakai__id: "c250e0204224c7ff234579f2282128359ddb89c8",
             jcr__primaryType: "",
             sakai__from: "Susan Hagstrom",
-            sakai__subject: "CED Commencement tickets",
+            "sakai:subject": "CED Commencement tickets",
             sakai__body: "Dear CED seniors, \n\n <b>Beginning Monday, April</b>",
             sakai__read: true,
             sakai__sendstate: "notified",
+            "sakai:taskstate": "created",
             sakai__to: "internal:eli",
-            sakai__dueDate: "2010-02-10T06:22:46-07:00",
+            "sakai:dueDate": "2010-02-10T06:22:46-07:00",
             sakai__completeDate: "",
             _charset_: "utf-8",
             id: "c250e0204224c7ff234579f2282128359ddb89c8",
@@ -137,12 +143,13 @@ sakai.myreminders = function(tuid, showSettings){
             sakai__id: "c250e0204224c7ff234579f2282128359ddb89c9",
             jcr__primaryType: "",
             sakai__from: "Susan Hagstrom",
-            sakai__subject: "Peer Adviser Workshop: Experience at Cal",
+            "sakai:subject": "Peer Adviser Workshop: Experience at Cal",
             sakai__body: "Wondering why our curriculum is the way it is?",
             sakai__read: true,
             sakai__sendstate: "notified",
+            "sakai:taskstate": "created",
             sakai__to: "internal:eli",
-            sakai__dueDate: "2010-08-10T06:22:46-07:00",
+            "sakai:dueDate": "2010-08-10T06:22:46-07:00",
             sakai__completeDate: "",
             _charset_: "utf-8",
             id: "c250e0204224c7ff234579f2282128359ddb89c9",
@@ -182,12 +189,13 @@ sakai.myreminders = function(tuid, showSettings){
             sakai__id: "c250e0204224c7ff234579f2282128359ddb88c8",
             jcr__primaryType: "",
             sakai__from: "Susan Hagstrom",
-            sakai__subject: "Getting too many emails?",
+            "sakai:subject": "Getting too many emails?",
             sakai__body: "Dear ARCH undergraduates, \n\n Following are some helpful",
             sakai__read: true,
             sakai__sendstate: "notified",
+            "sakai:taskstate": "created",
             sakai__to: "internal:eli",
-            sakai__dueDate: "2010-11-30T06:22:46-07:00",
+            "sakai:dueDate": "2010-11-30T06:22:46-07:00",
             sakai__completeDate: "",
             _charset_: "utf-8",
             id: "c250e0204224c7ff234579f2282128359ddb88c8",
@@ -226,12 +234,13 @@ sakai.myreminders = function(tuid, showSettings){
             sakai__id: "963b9e35158e98a289a5ddf646f892033bd8aae5",
             jcr__primaryType: "",
             sakai__from: "Susan Hagstrom",
-            sakai__subject: "April 23 deadline for CED commencement",
+            "sakai:subject": "April 23 deadline for CED commencement",
             sakai__body: "Dear CED seniors, \n\n If you would like to participate",
             sakai__read: true,
             sakai__sendstate: "notified",
+            "sakai:taskstate": "created",
             sakai__to: "internal:eli",
-            sakai__dueDate: "2010-04-23T06:22:46-07:00",
+            "sakai:dueDate": "2010-04-23T06:22:46-07:00",
             sakai__completeDate: "",
             _charset_: "utf-8",
             id: "963b9e35158e98a289a5ddf646f892033bd8aae5",
@@ -262,33 +271,85 @@ sakai.myreminders = function(tuid, showSettings){
         }]
     };
     
-    function test() {
-        alert("test");
-    }    
+//    function test() {
+//        alert("test");
+//    }    
     var fetchData = function(){
-        return reminders;
+        return mockreminders;
     };
     
     var createRemindersList = function(data){
-        $remindersList.html($.TemplateRenderer(myremindersTemplate, data));
+    	var htmlStr = $.TemplateRenderer("myreminders_template", data);
+        $remindersList.html(htmlStr);
     };
     
-    var getRemindersList = function(){
-        sakai.api.Widgets.loadWidgetData(tuid, function(success, data){
-            if (success) {
-                // load the user's reminders
-                createRemindersList(data);
-            }
-            else {
-                //alert("Error: Couldn't load reminders list from json [getRemindersList]");
-                var mockdata = fetchData();
-                createRemindersList(mockdata);
+    var getRemindersList = function(taskState, callback){
+    	var url = sakai.config.URL.MYREMINDERS_TASKSTATE_SERVICE + "?taskState=" + taskState;
+        $.ajax({
+            url: url,
+            cache: false,
+            success: function(data) {
+                if (data.results) {
+                	createRemindersList(merge(data, mockreminders));
+                }
+                else {
+                	createRemindersList(mockreminders);
+                }
+                if (typeof callback !== "undefined") {
+                    callback();
+                }
+
+            },
+            error: function(xhr, textStatus, thrownError) {
+            	alert("Getting Reminders failed for category=reminders and taskstate=" + taskState + " with status=" + textStatus + 
+            			" and thrownError=" + thrownError + "\n" + xhr.responseText);
             }
         });
     };
+
+    var merge = function(reminders1, reminders2) {
+    	if (typeof reminders1 !== "undefined") {
+    		if (typeof reminders2 !== "undefined") {
+    			reminders1.items = reminders1.items + reminders2.items;
+    			reminders1.total = reminders1.total + reminders2.total;
+    			reminders1.results.concat(reminders2.results);
+    			return reminders1;
+    		}
+    	}
+    	else {
+    		alert("Got no reminders to merge");
+    	}
+    };
+    
+    sakai.myreminders.taskDone = function(id, url) {
+      updateReminder(url, "taskState", "completed", function(){
+    	  $("#"+ id).slideUp("normal", function(){$(this).remove;});
+      });
+    }
+    
+    var updateReminder = function (url, propname, propvalue, callback) {
+    	var data = {};
+    	data[propname] = propvalue;
+    	$.ajax({
+    		  type: 'POST',
+    		  url: url,
+    		  data: data,
+    		  success: function(data, textStatus, xhr){
+    			  alert("updated " + propname + " to " + propvalue + "\n" + xhr.responseText);
+                  if (typeof callback !== "undefined") {
+                      callback();
+                  }
+    		  },
+              error: function(xhr, textStatus, thrownError) {
+              	alert("Updating " + url + " failed for " + propname + " = " + propvalue + " with status =" + textStatus + 
+              			" and thrownError = " + thrownError + "\n" + xhr.responseText);
+              },
+    		  dataType: 'json'
+    		});
+    };
     
     var doInit = function(){
-        getRemindersList();
+        getRemindersList("created");
     };
     
     doInit();
