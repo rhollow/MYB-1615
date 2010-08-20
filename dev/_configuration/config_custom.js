e9b1f0673d84cb91eab95714076b15f1aaf6ce92
elicochran <eli@media.berkeley.edu>
2010-08-20 15:26:03 -0700

    MYB-37: Changes to the tabs for the Home and Links pages

--- dev/_configuration/config_custom.js
+++ dev/_configuration/config_custom.js
@@ -3,3 +3,10 @@ var sakai = sakai || {};
 sakai.config = sakai.config || {};
 
 // Insert custom configuration here
+
+sakai.config.Navigation = [
+    {
+        "url" : "/dev/my_sakai.html",
+        "label" : "MY_SAKAI"
+    }
+];
