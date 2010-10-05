1d3991afe577716c097ba729a71202610c6f4bed
elicochran <eli@media.berkeley.edu>
2010-10-05 15:24:15 -0700

    fixed minor typo in inboxnotifier.html and created a better default for config_custom.js

--- dev/_configuration/config_custom.js
+++ dev/_configuration/config_custom.js
@@ -11,11 +11,11 @@ sakai.config.Navigation = [
     }
 ];
 
-// This is our custom CAS log in information, please note, we're not actually using the configuration yet. 
+// This is our custom CAS log in information. Please note, we're not actually using the external object yet 
 // For production and QA, sent internal to false, for dev set it to true.
 
 sakai.config.Authentication = {
-    "internal": false,
+    "internal": true,
     "external": [
         {
           label: "LOGIN_TITLE",
