7741bb4a3de7799baf966fa7896add35a8e04af9
Chris Tweney <chris@media.berkeley.edu>
2010-12-02 13:28:41 -0800

    MYB-364 - Functions in myberkeleysecurity aren't actually part of sakai.api.Security.

--- dev/javascript/myb/security.js
+++ dev/javascript/myb/security.js
@@ -21,31 +21,23 @@
 var sakai = sakai || {};
 sakai.myberkeleysecurity = function(){
 
-    // redefine some core Sakai API functions
-
-    sakai.api = sakai.api || {};
-    sakai.api.Security = sakai.api.Security || {};
-
     /**
-     * myberkeley custom function: Checks if the user is logged in
+     * Checks if the user is logged in
      * @return true if the user is logged in, false otherwise
      */
-    sakai.api.Security.isLoggedIn = function(){
+    var isLoggedIn = function(){
         var person = sakai.data.me;
         var uuid = person.user.userid;
-        if (!uuid || person.user.anon) {
-            return false;
-        }
-        return true;
+        return !(!uuid || person.user.anon);
     };
 
     /**
-     * myberkeley custom function: Check if the user is a myBerkeley participant.
+     * Check if the user is a myBerkeley participant.
      * There could be CED members who can log in, but are not myBerkeley participants.
      * This function checks 'sakai.data.me.profile.myberkeley.elements.participant' property.
      * @return true if the user is a myBerkeley participant, false otherwise
      */
-    sakai.api.Security.isMyBerkeleyParticipant = function(){
+    var isMyBerkeleyParticipant = function(){
         try {
             if (sakai.data.me.profile.myberkeley.elements.participant &&
                 sakai.data.me.profile.myberkeley.elements.participant.value === "true") {
@@ -59,28 +51,26 @@ sakai.myberkeleysecurity = function(){
     };
 
     /**
-     * myberkeley custom function: Function that can be called by pages that don't have the permission to show the content
+     * Function that can be called by pages that don't have the permission to show the content
      * they should be showing because the user in not a myBerkeley participant
      */
-    sakai.api.Security.sendToNotAMyBerkeleyParticipantPage = function(){
+    var sendToNotAMyBerkeleyParticipantPage = function(){
         var redurl = window.location.pathname + window.location.hash;
         document.location = "/dev/403_not_a_participant.html?redurl=" + escape(window.location.pathname + window.location.search + window.location.hash);
         return false;
     };
 
     var doInit = function() {
-        // Check if the user is logged in before loading data
-       var security = sakai.api.Security;
-       if (!security.isLoggedIn()) {
-           security.sendToLogin();
+       if (!isLoggedIn()) {
+           sendToLogin();
            return;
        }
 
        // If the user is a member of Berkeley's College of Environmental Design, but not a participant of myBerkeley project,
        // redirect him to the participation explanation page
-       if (!security.isMyBerkeleyParticipant()) {
-           security.sendToNotAMyBerkeleyParticipantPage();
-       } 
+       if (!isMyBerkeleyParticipant()) {
+           sendToNotAMyBerkeleyParticipantPage();
+       }
     };
 
     doInit();
