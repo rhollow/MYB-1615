16234f7c6fa641083acee03af5f322b8279b4b67
John King <johnk@media.berkeley.edu>
2011-01-18 13:56:20 -0800

    building dynamic list converter

--- dev/javascript/myb/upgrade/myb.upgradeDynamicLists.js
+++ dev/javascript/myb/upgrade/myb.upgradeDynamicLists.js
@@ -3,11 +3,28 @@
  */
 
 sakai.upgradeDynamicLists = function() {
-	var LIST_URL = "/private/dynamic_lists";
-	
+	var LIST_URL = "/private/dynamic_lists.infinity.json";
+    var dryRun = true;
+    
 	var getAdvisors = function() {
 		
 	}
 	
-	var loadList = 
-}
+	var loadLists = function() {
+		
+	}
+	
+    var doInit = function() {
+        var querystring = new Querystring();
+        if ( querystring.contains("dryRun") && querystring.get("dryRun") === "false") {
+            dryRun = false;
+        }
+        $("#upgrade_dynamiclists_button").live("click", function() {
+            console.log("Running upgrade; dryRun=" + dryRun);
+            loadLists();
+        });
+    };
+
+    doInit();
+};
+sakai.api.Widgets.Container.registerForLoad("sakai.upgradeDynamicLists");
