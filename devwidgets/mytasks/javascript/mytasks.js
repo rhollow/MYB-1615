fc267acf3d50e85a5e99c1d00067d06a76c675cb
Chris Tweney <chris@media.berkeley.edu>
2011-03-22 12:47:21 -0700

    MYB-417 Look at hasOverdueTasks to see whether to show the overdue-task UI

--- devwidgets/mytasks/javascript/mytasks.js
+++ devwidgets/mytasks/javascript/mytasks.js
@@ -112,51 +112,22 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/myb/myb.noticewidgets.js"],
             return "&start_date=" + Globalization.format(startDate, noticeWidgets.DATE_FORMAT_ISO8601)
                     + "&end_date=" + Globalization.format(endDate, noticeWidgets.DATE_FORMAT_ISO8601)
         };
-    
-        var checkForOverdueTasks = function() {
 
-            // TODO remove return when done debugging myb-417
-            return;
-
-
-            // TODO when KERN-1471 is fixed, bundle this request into batch request with the main search
-            var overdueTaskSearchURL = "/var/notices/tasks.json?startDate=" +
-                    Globalization.format(noticeWidgets.BEGINNING_OF_TIME, noticeWidgets.DATE_FORMAT_ISO8601)
-                    + "&endDate=" + Globalization.format(new Date(), noticeWidgets.DATE_FORMAT_ISO8601) +
-                    "&excludeRequiredState=completed&items=1";
-            $.ajax({
-                url: overdueTaskSearchURL,
-                cache: false,
-                success: function(data) {
-                    if ($.isArray(data.results) && data.results.length > 0) {
-                        $(".mytasks_overdue_tasks_msg", rootContainer).show();
-                        $(rootContainer).addClass("mytasks_overdue_tasks_exist");
-                    } else {
-						
-						if($(".mytasks_overdue_tasks_msg", rootContainer).is(":visible")){
-							$(".mytasks_overdue_tasks_msg", rootContainer).hide();	
-						}
-						if($(rootContainer).is(".mytasks_overdue_tasks_exist")){
-							$(rootContainer).removeClass("mytasks_overdue_tasks_exist");	
-						}						
-					}
-                },
-                error: function(xhr, textStatus, thrownError) {
-                    sakai.api.Util.notification.show("",
-                            sakai.api.i18n.Widgets.getValueForKey(widgetName, "default", "AN_ERROR_OCCURRED_CONTACTING_THE_SERVER"),
-                            sakai.api.Util.notification.type.ERROR, false);
-                    window.debug.error("Checking overdue tasks failed for:\n" + overdueTaskSearchURL + "\ncategory=reminders with status=" + textStatus +
-                            " and thrownError=" + thrownError + "\n" + xhr.responseText);
+        var onModelChange = function (model) {
+            if (!model.archiveMode) {
+                if (model.data.hasOverdueTasks) {
+                    $(".mytasks_overdue_tasks_msg", rootContainer).show();
+                    $(rootContainer).addClass("mytasks_overdue_tasks_exist");
+                } else {
+                    if ($(".mytasks_overdue_tasks_msg", rootContainer).is(":visible")) {
+                        $(".mytasks_overdue_tasks_msg", rootContainer).hide();
+                    }
+                    if ($(rootContainer).is(".mytasks_overdue_tasks_exist")) {
+                        $(rootContainer).removeClass("mytasks_overdue_tasks_exist");
+                    }
                 }
-            });
+            }
         };
-    
-		var onModelChange = function (model) {
-			if(!model.archiveMode){
-				checkForOverdueTasks();
-			}
-		};
-		
 	
         var doInit = function() {
             var taskWidget = noticeWidgets.Widget({
