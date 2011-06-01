b0f4693704ea350aa8845b95a6df750743afcaa8
Chris Tweney <chris@media.berkeley.edu>
2011-06-01 10:58:36 -0700

    MYB-636 Remove the sign up link for anon users

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -162,6 +162,9 @@ define(["/dev/configuration/config.js", "/dev/configuration/env.js"], function(c
     // so that user gets redirected to CAS logout
     config.followLogoutRedirects = true;
 
+    // remove the SIGN UP feature for anonymous users
+    delete(config.Navigation[4]);
+
     return config;
 
 });
