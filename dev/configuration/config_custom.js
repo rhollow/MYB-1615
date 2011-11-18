78d3e4602fa7092a04ccf70228025a53fad086b5
Eli Cochran <eli@media.berkeley.edu>
2011-11-18 15:44:52 -0800

    MYB-646, added the elements of the hybrid config to the custom_config file to add hybrid menu items and navigation structure.

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -20,7 +20,9 @@ define(["config/config", "config/env"], function(config) {
     // Insert custom configuration here
 
     // myberkeley custom begin
-
+    
+    // hybrid config is in the evironment specific env.js
+ 
     // This is our custom CAS log in information.
     config.Authentication.internal = config.isDev;
     config.Authentication.external = [
@@ -633,6 +635,43 @@ define(["config/config", "config/env"], function(config) {
             "selected": false
         }]
     };
+    
+    // Hybrid
+    
+    config.showSakai2=true;
+    config.useLiveSakai2Feeds=true;
+    
+    // adds the Sakai 2 Sites item to the left hand nav
+    config.defaultprivstructure["${refid}5"].dashboard.columns.column1.push({
+        "uid": "${refid}1234",
+        "visible": "block",
+        "name": "mysakai2"
+    });
+    
+    config.defaultprivstructure.structure0["sakai2sites"] =  {
+        "_ref": "${refid}2345",
+        "_title": "My Sakai 2 sites",
+        "_order": 2,
+        "_canEdit": true,
+        "_reorderOnly": true,
+        "_nonEditable": true,
+        "main": {
+            "_ref": "${refid}2345",
+            "_order": 0,
+            "_title": "My Sakai 2 sites"
+        }
+    };
+    
+    config.defaultprivstructure["${refid}2345"] = {
+        "page": "<div id='widget_searchsakai2_${refid}2346' class='widget_inline'></div>"
+    };
+    
+    // adds the My Sakai 2 Sites menu item to the Me menu
+    config.Navigation[0].subnav.splice(2,0, {
+        "url": "/me#l=sakai2sites",
+        "id": "subnavigation_sakai2_link",
+        "label": "MY_SAKAI2_SITES"
+    });
 
     // End custom configuration
 
