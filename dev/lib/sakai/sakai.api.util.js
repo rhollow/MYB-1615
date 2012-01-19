2ec274761d355a110bc2427cd5ec62edf147504f
Scot Hacker <shacker@berkeley.edu>
2012-01-18 16:57:59 -0800

    MYB-1296: Add aria-hidden and aria-required attribs to profile editor so that asterisks and legend aren't read to screen reader users.

--- dev/lib/sakai/sakai.api.util.js
+++ dev/lib/sakai/sakai.api.util.js
@@ -1699,6 +1699,9 @@ define(
                 html4.ATTRIBS["a::aria-haspopup"] = 0;
                 html4.ATTRIBS["a::role"] = 0;
                 html4.ATTRIBS["ul::aria-hidden"] = 0;
+                html4.ATTRIBS["span::aria-hidden"] = 0;
+                html4.ATTRIBS["p::aria-hidden"] = 0;                
+                html4.ATTRIBS["input::aria-required"] = 0;                
                 html4.ATTRIBS["ul::role"] = 0;
 
                 // A slightly modified version of Caja's sanitize_html function to allow style="display:none;"
